562bd0f674448fec86378599fb0d7339a408cfc3
Fabrício R. de França <fabriciobtcc@gmail.com>
2022-03-20 16:02:56 -0300

    adicionado os pacotes view e model e adicionado a classe aluno e a classe inicial da view

---
+++ src/projetojava/model/Aluno.java
@@ -0,0 +1,109 @@
+package projetojava.model;
+
+public class Aluno {
+
+	
+	/* atributos*/
+	private String nome;
+	private int idade;
+	private String dataNascimneto;
+	private String RG;
+	private String CPF;
+	private String nomeMae;
+	private String nomePai;
+	private String dataMatricula;
+	
+	
+	/* construtor*/
+    public Aluno() { 
+		
+		
+	}
+
+
+    /* getters setters */
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public int getIdade() {
+		return idade;
+	}
+
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+
+	public String getDataNascimneto() {
+		return dataNascimneto;
+	}
+
+
+	public void setDataNascimneto(String dataNascimneto) {
+		this.dataNascimneto = dataNascimneto;
+	}
+
+
+	public String getRG() {
+		return RG;
+	}
+
+
+	public void setRG(String rG) {
+		RG = rG;
+	}
+
+
+	public String getCPF() {
+		return CPF;
+	}
+
+
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+
+
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+    
+    
+    
+	
+	
+	
+}
---
+++ src/projetojava/view/PrimeiraClasse.java
@@ -0,0 +1,17 @@
+package projetojava.view;
+
+import projetojava.model.Aluno;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		
+		
+		/* instancia __ objeto aluno*/
+		Aluno aluno = new Aluno();
+		
+		
+
+	}
+
+}
