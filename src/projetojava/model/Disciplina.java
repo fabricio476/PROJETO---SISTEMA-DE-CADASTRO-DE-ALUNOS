4060d75bb12324f450d2ca6503610c8895f7eab9
Fabrício R. de França <fabriciobtcc@gmail.com>
2022-03-25 22:03:06 -0300

    Classe Disciplina

---
+++ src/projetojava/model/Disciplina.java
@@ -0,0 +1,46 @@
+package projetojava.model;
+
+public class Disciplina {
+	
+	/*parametro*/
+	private String nomeDisciplina;
+	private String notaDisciplina;
+	
+	/*construtor*/
+	public Disciplina() {
+		
+		
+	}
+
+	
+	/* getters e setters*/
+	public String getNomeDisciplina() {
+		return nomeDisciplina;
+	}
+
+	public void setNomeDisciplina(String nomeDisciplina) {
+		this.nomeDisciplina = nomeDisciplina;
+	}
+
+	public String getNotaDisciplina() {
+		return notaDisciplina;
+	}
+
+	public void setNotaDisciplina(String notaDisciplina) {
+		this.notaDisciplina = notaDisciplina;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
