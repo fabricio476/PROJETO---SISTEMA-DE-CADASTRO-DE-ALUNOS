e12ffec1ba22483437ceaa8bf0d0c681b5a0ae1f
Fabrício R. de França <fabriciobtcc@gmail.com>
2022-05-12 23:25:50 -0300

    imlemnetado a interface

--- src/projetojava/dal/SecretarioDAOImpl.java
+++ src/projetojava/dal/SecretarioDAOImpl.java
@@ -1,8 +1,68 @@
 package projetojava.dal;
 
+import java.util.List;
 
+import projetojava.interfaces.SecretarioDAO;
+import projetojava.model.Aluno;
+import projetojava.model.Disciplina;
 
-public class SecretarioDAOImpl {
+public class SecretarioDAOImpl implements SecretarioDAO{
+	
+	
+	
+
+	@Override
+	public boolean cadastrarAluno(Aluno aluno) {
+		
+		
+		
+		
+		
+		
+		return false;
+	}
+
+	@Override
+	public List<Aluno> listarAlunosCurso() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean editarAluno(Aluno aluno) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean excluirAluno(Aluno aluno) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Aluno buscarPorNome(String nome) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean cadstrarDisciplina(Disciplina disciplina) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Disciplina> listarDisciplinas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean aditarDisciplina(Disciplina Disciplina) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 	
 	
