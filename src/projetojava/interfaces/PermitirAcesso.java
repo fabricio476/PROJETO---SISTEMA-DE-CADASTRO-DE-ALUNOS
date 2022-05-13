2fea17a970eea7d8964ae975649fb8a01ade08ef
Fabrício R. de França <fabriciobtcc@gmail.com>
2022-05-12 23:13:14 -0300

    adicionado metodos na interface

--- src/projetojava/interfaces/PermitirAcesso.java
+++ src/projetojava/interfaces/PermitirAcesso.java
@@ -1,11 +1,13 @@
 package projetojava.interfaces;
 
-import projetojava.model.Pessoa;
+
 
 public interface PermitirAcesso {
 	
 	
 	
 	public boolean autenticar(String login, String senha);
+	
+	
 
 }
--- src/projetojava/interfaces/SecretarioDAO.java
+++ src/projetojava/interfaces/SecretarioDAO.java
@@ -4,19 +4,29 @@ package projetojava.interfaces;
 import java.util.List;
 
 import projetojava.model.Aluno;
+import projetojava.model.Disciplina;
 
 public interface SecretarioDAO {
 
 	
 	 public boolean cadastrarAluno(Aluno aluno);
 	 
-	 public List<Aluno> listarAlunos();
+	 public List<Aluno> listarAlunosCurso();
 	 
 	 public boolean editarAluno(Aluno aluno);
 	 
 	 public boolean excluirAluno(Aluno aluno);
+	 
+	 public Aluno buscarPorNome(String nome);
+	 
 		
 //------------------------------------------------------------------------	 
+	 
+	 public boolean cadstrarDisciplina(Disciplina disciplina);
+	 
+	 public List<Disciplina> listarDisciplinas();
+	 
+	 public boolean aditarDisciplina(Disciplina Disciplina);
 	
 	
 	
